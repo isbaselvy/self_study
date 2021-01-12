d5e496406dc1499d9dff4b4d5432a5c951bcec2f
isbaselvvy <997666543@qq.com>
2021-01-12 20:08:13 +0800

    结构赋值-对象、字符串、数值和布尔、函数参数

--- jiegoufuzhi.js
+++ jiegoufuzhi.js
@@ -53,3 +53,113 @@ let obj = {
 }
 // const {key1, key2, key3: [skill]} = obj // 获取不到key3
 const {key1, key2, key3: [{skillName}, {skillName:skillName2}]} = obj // skillName "js", skillName2 "java"
+
+// 对象的解构赋值
+// 结合扩展运算符 
+// 取值
+// const obj = {
+//     name: 'xiaoming',
+//     age: 19,
+//     gender: 'male',
+//     love: ['dad', 'mom']
+// }
+
+// const {name, ...oth} = obj
+// // console.log(name, oth) // xiaoming {age: 19, gender: "male"}
+
+// // 合并对象
+// let obj2 = {interest: 'fottball', ...obj}
+// console.log(obj2) // {interest: "fottball", name: "xiaoming", age: 19, gender: "male"}
+// obj.name = 'xiaofang'
+// console.log(obj2) // name不变 {interest: "fottball", name: "xiaoming", age: 19, gender: "male"}
+
+// obj.love[0] = ['hhh']
+// console.log(obj2) // love改变， ...合并对象，对元素非引用类型时是深拷贝，反正浅拷贝
+
+// 如何向已声明的变量进行解构赋值
+// let age;
+// const obj = {
+//     name: 'xiaoming',
+//     age: 19,
+//     gender: 'male'
+// };
+// // { age } = obj // 报错,此时{}被当作的块级作用域
+// ({ age } = obj) // 不推荐，作为了解
+// console.log(age) // 19
+
+// // 默认值
+// const {name, hobby = '学习', gf = undefined } = obj
+// console.log(name, hobby, gf) // xiaoming 学习 undefined
+
+// 常用场景
+// 1.提取对象属性
+// const obj = {
+//     name: 'xiaoming',
+//     age: 19,
+//     gender: 'male',
+//     love: ['dad', 'mom']
+// }
+// const {name, love : hobby, love: [dad] } = obj
+// console.log(name, hobby, dad) // xiaoming (2) ["dad", "mom"] dad
+
+// 2.传入乱序的函数参数
+// function AJAX({
+//     url,
+//     param,
+//     type = 'get'
+// }) {
+//     console.log(type) // get
+// }
+
+// AJAX({
+//     param: {id: 1},
+//     url:'/getInfo'
+// })
+
+// 3.获取多个函数返回值
+// function getInfo(id) {
+//     return {
+//         name: 'xiaoming',
+//         data: {id},
+//         msg:'succeess'
+//     }
+// }
+
+// const {name, data, msg:message} = getInfo(1)
+// console.log(name, data, message) // xiaoming {id: 1} succeess
+
+// 字符串的解构赋值
+// let str = 'i am jack'
+// const [a, b, ...oth] = str
+// console.log(a, b, oth) // i b是个空  (7) ["a", "m", " ", "j", "a", "c", "k"]
+
+// // str1, str2, str3等价 ["i", " ", "a", "m", " ", "j", "a", "c", "k"]
+// const str1 = [...str]
+// const str2 = str.split("")
+// const [...str3] = str
+
+// // 提取属性
+// const {length, split} = str
+// let test = split.call('1,2,3', '')
+// console.log(test)
+
+// 数值与布尔值的结构赋值
+// const { valueOf } = 1 
+// // valueOf输出是一个函数，1本身没有valueOf方法，调用会报错，此处 可解析出来是结构赋值时会先将数值转成一个包装对象 number
+// // Number(1).valueOf() 1, 1.valueOf 报错
+// const { toString } = false
+// let str = 'jack'
+// console.log(str.length) // 4
+// str.a = '1'
+// console.log(str.a) // undefined
+
+// 函数参数的解构赋值
+// function swap([x, y]) {
+//     return [y, x]
+// }
+// const [a, b] = swap([3, 4])  // a 4, b 3
+// 以下这种同上，因args是一个类数组，所以自动结构赋值？
+// function swap(x, y) {
+//     return [y, x]
+// }
+// const [a, b] = swap(3, 4)  // a 4, b 3
