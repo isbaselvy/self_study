d82b8f1136448b7c316c1b76b283777b005c690c
isbaselvy <997666543@qq.com>
2021-05-07 17:23:23 +0800

    vue3变化

---
+++ vue3/test.vue
@@ -0,0 +1,16 @@
+import { ref, onBeforeUpdate } from 'vue'
+export default {
+    setup() {
+        let itemRefs = []
+        const setItemRefs = el => {
+            itemRefs.push(el)
+        }
+        onBeforeUpdate(()=>{
+            itemRefs = []
+        })
+        return {
+            itemRefs,
+            setItemRefs
+        }
+    }
+}
